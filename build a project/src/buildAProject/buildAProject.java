df5add0fd5d3190c0877de52c4c207b4e5877dba
sarahbickle80 <59835806+sarahbickle80@users.noreply.github.com>
2020-02-15 14:13:08 +0000

    added project files
    1. added project files for java

---
+++ build a project/src/buildAProject/buildAProject.java
@@ -0,0 +1,9 @@
+package buildAProject;
+
+public class buildAProject {
+	public static void main (String[] orgs) {
+		System.out.println("This is my first program!");
+		System.out.println("Programmers are Rockstars!");
+	}
+
+}
